8f2410b22942b01f77cb33404416155d7e669596
hojong <33508056+LteFroggy@users.noreply.github.com>
2025-07-27 14:23:50 +0900

    feat(MyPage) : 주석 추가 (#5)

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Typography } from 'antd';
+import { Button, Divider, Modal, Typography } from 'antd';
 import { useProfile } from './hooks/useProfile.ts';
 import {
   Container,
@@ -13,7 +13,6 @@ import { useNavigate } from 'react-router';
 // MyPage() => {} 구문은 화살표 함수라고 하며, const(변수)를 함수처럼 사용할 수 있게 한다
 // 덕분에 MyPage()는 변수이면서, 사용했을 때 함수처럼 return을 줌
 export const MyPage = () => {
-  const navigate = useNavigate();
 
   // 객체에서 값을 꺼내기 위해 중괄호로 profile을 묶은 것이다.
   // useProfile이 객체를 반환한다고 생각하면, 그 중에 profile 이라는 값만 꺼낼 수 있게 함
@@ -24,26 +23,30 @@ export const MyPage = () => {
     return <></>;
   }
   return (
-    <Container>
-      <ProfileSection>
-        <ImageContainer>
-          <ProfileImage src={profile.myImage} alt='profileImage' />
-          <Button type='default' onClick={() => navigate('/mypage/modify')} size='small'>
-            프로필 수정
-          </Button>
-        </ImageContainer>
-        <ProfileTexts>
-          <Typography.Title
-            level={3}
-          >{`${profile.name}님, 오늘도 편안한 독서 되세요.`}</Typography.Title>
-          {/* 가입일? 데이터 없음 */}
-          {/* <Typography.Paragraph>
-          오늘은 Mookbook과 함께한지 ?일 되는 날이예요.
-        </Typography.Paragraph> */}
-        </ProfileTexts>
-      </ProfileSection>
-      <Divider />
-      <BookmarkSection />
-    </Container>
+    <>
+      <Container>
+        <ProfileSection>
+          <ImageContainer>
+            <ProfileImage src={profile.myImage} alt='profileImage' />
+            <Button type='default' onClick={onEditProfileClick} size='small'>
+              프로필 수정
+            </Button>
+          </ImageContainer>
+          <ProfileTexts>
+            <Typography.Title
+              level={3}
+            >{`${profile.name}님, 오늘도 편안한 독서 되세요.`}</Typography.Title>
+            {/* 가입일? 데이터 없음 */}
+            {/* <Typography.Paragraph>
+            오늘은 Mookbook과 함께한지 ?일 되는 날이예요.
+          </Typography.Paragraph> */}
+          </ProfileTexts>
+        </ProfileSection>
+        <Divider />
+        <BookmarkSection />
+      </Container>
+      <Modal open={isEditModalShown} onCancel={() => setEditModalShown(false)}>
+      </Modal>
+    </>
   );
 };
