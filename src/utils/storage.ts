79670c51fa9c5d005d16c7c3c55d578684180ed5
hojong <33508056+LteFroggy@users.noreply.github.com>
2025-07-27 14:22:53 +0900

    feat(storage) : Mood_KEYS 타입 추가 및 주석 추가 (#5)

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,11 +1,19 @@
+// 상수 객체이다. 세 개의 key-value 쌍을 가진 Map으로 봐도 될 듯
+// as const를 붙인 이유는, readonly로 만들기 위함이다. 해당 값 내부는 절대 수정 못 함
 export const StorageKeys = {
   ACCESS_TOKEN: 'ACCESS_TOKEN',
   REFRESH_TOKEN: 'REFRESH_TOKEN',
+  MOOD_KEYS: 'MOOD_KEYS'
 } as const;
-export type StorageKey = keyof typeof StorageKeys;
+
+// StorageKeys 객체의 key 이름들(ACCESS_TOKEN, REFRESH_TOKEN, MOOD_KEYS)을 모아 새로운 타입으로 만든다.
+export type StorageKey =
+  // 'typeof StorageKeys'로 객체 타입을 가져오고, 'keyof'를 통해 키 이름들만 추출한다.
+  keyof typeof StorageKeys;
 
 export const getLocalStorageItem = (key: StorageKey) => {
   try {
+    // a ?? b => a가 null/undefined라면 b를 반환
     const value = localStorage.getItem(key) ?? '"null"';
     return JSON.parse(value);
   } catch {
